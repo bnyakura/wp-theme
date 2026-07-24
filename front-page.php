d6a565e5f7a668ea1881daf979018b7f00752fc6
Brandon Nyakura <bnyakura@oes.edu.au>
2026-07-24 15:57:43 +0200

    remove wisdom files in inc directory

--- front-page.php
+++ front-page.php
@@ -10,15 +10,6 @@ get_header();
 
 <main id="primary">
 
-	<?php get_template_part( 'template-parts/hero' ); ?>
-	<?php get_template_part( 'template-parts/statsBar' ); ?>
-	<?php get_template_part( 'template-parts/Distinction' ); ?>
-	<?php get_template_part( 'template-parts/Mission' ); ?>
-	<?php get_template_part( 'template-parts/Forge' ); ?>
-	<?php get_template_part( 'template-parts/Pricing' ); ?>
-	<?php get_template_part( 'template-parts/Testimonials' ); ?>
-	<?php get_template_part( 'template-parts/Contact' ); ?>
-
 	<?php
 	if ( have_posts() ) :
 		while ( have_posts() ) :
